6d4b1a529db342bad1563e19e0edbc91937d199c
Snehalrohidas-28 <snehalrohidas012@gmail.com>
2026-04-07 17:24:28 +0530

    Create ProfileActivity.java

---
+++ Assignment_No_5/Intent/Explicit/ProfileActivity.java
@@ -0,0 +1,24 @@
+package com.example.profile;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Profile extends AppCompatActivity {
+
+    Button btnBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.activity_profile);
+
+        btnBack = findViewById(R.id.btnBack);
+
+   
+        btnBack.setOnClickListener(v -> finish());
+    }
+}
