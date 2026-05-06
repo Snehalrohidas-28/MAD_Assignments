45156662534726b771d7cdfaf87e305912cb22f7
Snehalrohidas-28 <snehalrohidas012@gmail.com>
2026-05-06 22:07:05 +0530

    Create User.java

---
+++ Project/FoodieFast/models/User.java
@@ -0,0 +1,18 @@
+package com.example.foodiefast.models;
+
+public class User {
+
+    public String uid;
+    public String name;
+    public String email;
+    public String phone;
+
+    public User() {}
+
+    public User(String uid, String name, String email, String phone) {
+        this.uid = uid;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+}
